7d7c8d40edeea1261c97ab1332bea02921d8c44a
centino001 <centino001@gmail.com>
2023-03-23 16:00:22 +0100

    a simple card ID

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      home: eekayCard(),
+    ));
+
+class eekayCard extends StatefulWidget {
+  const eekayCard({super.key});
+
+  @override
+  State<eekayCard> createState() => _eekayCardState();
+}
+
+class _eekayCardState extends State<eekayCard> {
+  int ninjaLevel = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: const Text('ESDIGITECH ID card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel += 1;
+          });
+        },
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/emoji.jpeg'),
+                radius: 40.0,
+              ),
+            ),
+            const Divider(
+              height: 60.0,
+              color: Colors.white,
+            ),
+            const Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Text(
+              'Eekay',
+              style: TextStyle(
+                color: Colors.red,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            const Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '$ninjaLevel',
+              style: const TextStyle(
+                color: Colors.red,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                const SizedBox(
+                  width: 9.0,
+                ),
+                Text(
+                  'eekay@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
